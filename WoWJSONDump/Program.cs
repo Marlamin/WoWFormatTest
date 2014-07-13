d42dfe240ec1966496237f314774dd00d784e35b
Martin Benjamins <marlamin@marlamin.com>
2014-07-13 15:42:20 +0200

    Add JSON dumping application to test results

---
+++ WoWJSONDump/Program.cs
@@ -0,0 +1,22 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WoWFormatLib;
+using WoWFormatLib.FileReaders;
+
+namespace WoWJSONDump
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            M2Reader reader = new M2Reader("Z:\\WoD\\18522_full\\");
+            reader.LoadM2(@"Creature\Alakir\AlAkir.m2");
+            File.WriteAllText(@"Z:\WoD\model.json", JsonConvert.SerializeObject(reader.model, Formatting.Indented));
+        }
+    }
+}
