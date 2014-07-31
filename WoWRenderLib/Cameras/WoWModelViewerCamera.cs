1b6c2ed6e55947f494ee33bd23c0f2d940f6d07e
Olivier Beaudoin <olivier@thoorium.com>
2014-07-31 10:08:43 -0400

    WoWRenderLib project

---
+++ WoWRenderLib/Cameras/WoWModelViewerCamera.cs
@@ -0,0 +1,14 @@
+﻿using SharpDX.WPF.Cameras;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WoWRenderLib
+{
+    public class WoWModelViewerCamera : BaseCamera
+    {
+        //TODO: Move my modifications from BaseCamera here
+    }
+}
