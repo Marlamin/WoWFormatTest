c67e6729c02adfe0cd55d87c4bdf3b71e14851d0
Martin Benjamins <marlamin@marlamin.com>
2014-10-13 16:01:34 +0200

    Placeholder CASC functionality in lib

---
+++ WoWFormatLib/Utils/CASC.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CASCExplorer;
+using System.IO;
+
+namespace WoWFormatLib.Utils
+{
+    class CASC
+    {
+        public static CASCHandler cascHandler;
+        private static LocaleFlags locale = LocaleFlags.All;
+        private static ContentFlags content = ContentFlags.None;
+
+        public static void InitCasc(){
+            cascHandler = CASCHandler.OpenOnlineStorage("wow_beta");
+        }
+
+        public static void GenerateListfile()
+        {
+            //extract signaturefile and extract list of some files from that
+            cascHandler.SaveFileTo("signaturefile", "data/", locale, content);
+            FileStream stream = File.Open("data/signaturefile", FileMode.Open);
+        }
+
+        public static void DownloadFile()
+        {
+            cascHandler.SaveFileTo("signaturefile", "data/", locale, content);
+        }
+    }
+}
