6f721eda3c4adac7e89274c792dffcc5d5682965
Martin Benjamins <marlamin@marlamin.com>
2015-08-02 15:38:12 +0200

    Add latest files

---
+++ WoWFormatLib/Structs/WDT.struct.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WoWFormatLib.Structs.WDT
+{
+    public struct WDL
+    {
+       // public MWMO mwno; //WMO filenames (zero terminated)
+
+    }
+
+  //  public struct MWMO
+  //  {
+//
+  //  }
+}
