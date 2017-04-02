c3581ec515021b1342ad499eb6c7ad45030b8f7f
Martin Benjamins <marlamin@marlamin.com>
2017-04-02 22:21:33 +0200

    Misc changes

--- DBCtest/Program.cs
+++ DBCtest/Program.cs
@@ -1,7 +1,9 @@
-﻿using System;
+﻿
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 using WoWFormatLib;
@@ -10,28 +12,55 @@ using WoWFormatLib.Utils;
 
 namespace DBCtest
 {
+     class DatWebClient : WebClient
+    {
+        protected override WebRequest GetWebRequest(Uri uri)
+        {
+            WebRequest w = base.GetWebRequest(uri);
+            w.Timeout = 5000;
+            return w;
+        }
+    }
+
     class Program
     {
         static void Main(string[] args)
         {
-            CASC.InitCasc();
+            CASC.InitCasc(null, "C:\\World of Warcraft", "wow");
 
-            DB2Reader<AreaPOIRecord> reader = new DB2Reader<AreaPOIRecord>("DBFilesClient\\AreaPOI.db2");
-            for (int i = 0; i < reader.recordCount; i++)
+            var creatureData = new DBFilesClient.NET.Storage<CreatureEntry>(CASC.OpenFile(@"DBFilesClient/Creature.db2"));
+            var cdiData = new DBFilesClient.NET.Storage<CreatureDisplayInfoEntry>(CASC.OpenFile(@"DBFilesClient/CreatureDisplayInfo.db2"));
+
+            using (DatWebClient client = new DatWebClient())
             {
-                Console.WriteLine(reader[i].ID + ": " + reader[i].name_lang);
-            }
-                //DBCReader<AreaTableRecord> reader = new DBCReader<AreaTableRecord>();
-                //reader.LoadDBC("DBFilesClient\\AreaTable.dbc");
-                /*Console.WriteLine(reader.header.record_count + " rows!");
-                Console.WriteLine(reader.header.record_size + " row size!");
-                Console.WriteLine(reader.header.field_count + " fields!");
-                at.rows
-                for (int i = 0; i < at.records.Count(); i++)
+                foreach (var entry in creatureData)
                 {
-                 //Console.WriteLine("Area: " + reader.stringblock[(int)reader.records[i].AreaName_lang] + " <" + reader.stringblock[(int)reader.records[i].ZoneName] +">");
-                }*/
-                Console.ReadLine();
+                    if (File.Exists("Creature/" + entry.Key + ".png")) continue;
+                    if (entry.Value.DisplayID[0] == 0) continue;
+                    try
+                    {
+                        client.DownloadFile(new Uri("http://localhost:12345/wow/creature/" + entry.Key), "Creature/" + entry.Key + ".png");
+                    }
+                    catch
+                    {
+
+                    }
+                }
+
+                foreach (var entry in cdiData)
+                {
+                    if (File.Exists("CDI/" + entry.Key + ".png")) continue;
+                    if (entry.Value.ModelID == 0) continue;
+                    try
+                    {
+                        client.DownloadFile(new Uri("http://localhost:12345/wow/creatureDisplayInfo/" + entry.Key), "CDI/" + entry.Key + ".png");
+                    }
+                    catch
+                    {
+
+                    }
+                }
+            }
         }
     }
 }
