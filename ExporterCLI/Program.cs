94a2ecf8e67ddff640cbf799adb23dd54465ef68
Martin Benjamins <marlamin@marlamin.com>
2018-02-13 21:03:48 +0100

    Require program as first argument

--- ExporterCLI/Program.cs
+++ ExporterCLI/Program.cs
@@ -8,34 +8,34 @@ namespace ExporterCLI
     {
         static void Main(string[] args)
         {
-            if(args.Length < 2)
+            if(args.Length < 3)
             {
-                Console.WriteLine("Not enough arguments, needs filename, outdir and type (if filedataid)");
+                Console.WriteLine("Not enough arguments, needs program, filename, outdir and type (if filedataid)");
                 return;
             }
 
-            CASC.InitCasc(null, null, "wowz");
+            CASC.InitCasc(null, null, args[0]);
 
-            var target = args[0].Replace('/', '\\');
+            var target = args[1].Replace('/', '\\');
 
-            var ext = Path.GetExtension(args[0]).ToLower();
+            var ext = Path.GetExtension(args[1]).ToLower();
             switch (ext){
                 case ".adt":
-                    Exporters.glTF.ADTExporter.ExportADT(target, args[1]);
+                    Exporters.glTF.ADTExporter.ExportADT(target, args[2]);
                     break;
                 case ".wmo":
-                    Exporters.glTF.WMOExporter.ExportWMO(target, null, args[1]);
+                    Exporters.glTF.WMOExporter.ExportWMO(target, null, args[2]);
                     break;
                 default:
-                    if(args.Length == 3)
+                    if(args.Length == 4)
                     {
                         Console.WriteLine("Has type, assuming filedataid");
-                        if(args[2] == "wmo")
+                        if(args[3] == "wmo")
                         {
                             var isNumeric = int.TryParse(target, out var filedataid);
                             if (isNumeric)
                             {
-                                Exporters.glTF.WMOExporter.ExportWMO("wmo_" + args[0] + ".wmo", null, args[1], filedataid);
+                                Exporters.glTF.WMOExporter.ExportWMO("wmo_" + args[1] + ".wmo", null, args[2], filedataid);
                             }
                         }
                     }
