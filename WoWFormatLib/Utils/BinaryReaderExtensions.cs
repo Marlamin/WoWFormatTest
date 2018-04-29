f34bb782f5392070ee4d675e3721d9e55bd4cfd8
Martin Benjamins <marlamin@marlamin.com>
2018-04-29 15:26:48 +0200

    Move BinaryReader extensions to separate file

---
+++ WoWFormatLib/Utils/BinaryReaderExtensions.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using CASCLib;
+
+namespace WoWFormatLib
+{
+    public static class Extensions
+    {
+        public static int ReadInt32BE(this BinaryReader reader)
+        {
+            byte[] val = reader.ReadBytes(4);
+            return val[3] | val[2] << 8 | val[1] << 16 | val[0] << 24;
+        }
+
+        public static void Skip(this BinaryReader reader, int bytes)
+        {
+            reader.BaseStream.Position += bytes;
+        }
+
+        public static uint ReadUInt32BE(this BinaryReader reader)
+        {
+            byte[] val = reader.ReadBytes(4);
+            return (uint)(val[3] | val[2] << 8 | val[1] << 16 | val[0] << 24);
+        }
+
+        public static T Read<T>(this BinaryReader reader) where T : struct
+        {
+            byte[] result = reader.ReadBytes(FastStruct<T>.Size);
+
+            return FastStruct<T>.ArrayToStructure(result);
+        }
+
+        public static T[] ReadArray<T>(this BinaryReader reader) where T : struct
+        {
+            int numBytes = (int)reader.ReadInt64();
+
+            byte[] result = reader.ReadBytes(numBytes);
+
+            reader.BaseStream.Position += (0 - numBytes) & 0x07;
+            return FastStruct<T>.ReadArray(result);
+        }
+
+        public static T[] ReadArray<T>(this BinaryReader reader, int size) where T : struct
+        {
+            int numBytes = FastStruct<T>.Size * size;
+
+            byte[] result = reader.ReadBytes(numBytes);
+
+            return FastStruct<T>.ReadArray(result);
+        }
+
+        public static short ReadInt16BE(this BinaryReader reader)
+        {
+            byte[] val = reader.ReadBytes(2);
+            return (short)(val[1] | val[0] << 8);
+        }
+
+        public static string ToHexString(this byte[] data)
+        {
+            return BitConverter.ToString(data).Replace("-", string.Empty);
+        }
+
+        public static byte[] Copy(this byte[] array, int len)
+        {
+            byte[] ret = new byte[len];
+            for (int i = 0; i < len; ++i)
+                ret[i] = array[i];
+            return ret;
+        }
+
+        public static string ToBinaryString(this BitArray bits)
+        {
+            StringBuilder sb = new StringBuilder(bits.Length);
+
+            for (int i = 0; i < bits.Length; ++i)
+            {
+                sb.Append(bits[i] ? "1" : "0");
+            }
+
+            return sb.ToString();
+        }
+    }
+
+    public static class CStringExtensions
+    {
+        /// <summary> Reads the NULL terminated string from 
+        /// the current stream and advances the current position of the stream by string length + 1.
+        /// <seealso cref="BinaryReader.ReadString"/>
+        /// </summary>
+        public static string ReadCString(this BinaryReader reader)
+        {
+            return reader.ReadCString(Encoding.UTF8);
+        }
+
+        /// <summary> Reads the NULL terminated string from 
+        /// the current stream and advances the current position of the stream by string length + 1.
+        /// <seealso cref="BinaryReader.ReadString"/>
+        /// </summary>
+        public static string ReadCString(this BinaryReader reader, Encoding encoding)
+        {
+            var bytes = new List<byte>();
+            byte b;
+            while ((b = reader.ReadByte()) != 0)
+                bytes.Add(b);
+            return encoding.GetString(bytes.ToArray());
+        }
+
+        public static void WriteCString(this BinaryWriter writer, string str)
+        {
+            var bytes = Encoding.UTF8.GetBytes(str);
+            writer.Write(bytes);
+            writer.Write((byte)0);
+        }
+
+        public static byte[] ToByteArray(this string str)
+        {
+            str = str.Replace(" ", string.Empty);
+
+            var res = new byte[str.Length / 2];
+            for (int i = 0; i < res.Length; ++i)
+            {
+                res[i] = Convert.ToByte(str.Substring(i * 2, 2), 16);
+            }
+            return res;
+        }
+    }
+}
